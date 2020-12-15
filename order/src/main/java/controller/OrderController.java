a03fc59c1c165dd40d25bf682c55aedab2868c4f
yuqilin228 <1481595562@qq.com>
2020-12-15 09:51:57 +0800

    222修改了controller

--- order/src/main/java/controller/OrderController.java
+++ order/src/main/java/controller/OrderController.java
@@ -2,6 +2,7 @@ package controller;
 
 public class OrderController {
     public void hello(){
-        System.out.println("222操作");
+        System.out.println("b操作01");
+        System.out.println("b操作02");
     }
 }
