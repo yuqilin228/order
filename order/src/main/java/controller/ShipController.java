0318643ba30d5360c188b5e47e75d93d230ae5d8
yuqilin228 <1481595562@qq.com>
2020-12-18 00:28:03 +0800

    ljt 提交了shipController代码2

--- order/src/main/java/controller/ShipController.java
+++ order/src/main/java/controller/ShipController.java
@@ -3,5 +3,6 @@ package controller;
 public class ShipController {
     public static void main(String[] args) {
         System.out.println("ljt分支提交的ShipController");
+        System.out.println("ljt分支提交的ShipController1");
     }
 }
