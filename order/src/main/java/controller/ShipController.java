b3c10145d68d42e6372f38524f543c61af41ad84
yuqilin228 <1481595562@qq.com>
2020-12-18 20:54:20 +0800

    aaa ljt 分支删除了文件

--- order/src/main/java/controller/ShipController.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-public class ShipController {
-    public static void main(String[] args) {
-        System.out.println("ljt分支提交的ShipController");
-        System.out.println("ljt分支提交的ShipController1");
-        System.out.println("ljt分支提交的ShipController2");
-    }
-}
--- order/src/main/java/controller/ShipController2.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-public class ShipController2 {
-    public static void main(String[] args) {
-
-    }
-}
