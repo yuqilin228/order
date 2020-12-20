0b754612212817e89e7b0979e76e6baafbb6bf44
yuqilin228 <1481595562@qq.com>
2020-12-20 19:36:37 +0800

    bbb ljt分支 初始化代码

--- order/OrderController.java
+++ order/OrderController.java
@@ -1,11 +1,5 @@
 public class OrderController {
     public static void main(String[] args) {
-        System.out.println("aaa修改了代码111");
-        System.out.println("bbb修改了代码222");
-        System.out.println("aaa修改了代码222");
-
 
     }
-
-
 }
